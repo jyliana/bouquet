f998f10de1b00f76920a7bc713327547588908ef
Jyliana <jyliana@gmail.com>
2017-07-14 00:10:05 +0300

    Changed Mathilde

--- bouquet/src/flowers/MathildeRose.java
+++ bouquet/src/flowers/MathildeRose.java
@@ -4,20 +4,29 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
+/**
+* New comment
+*/
+
 /**
  * Created by Inna on 8/13/16.
  */
 public class MathildeRose extends DecorativeFlower
 {
-   /* public MathildeRose(Color color)
+   /*
+    protected Color calcColor()
+    {
+        return getRandomColorFromAllowed();
+    }
+
+    public MathildeRose(Color color)
+
     {
         super(color);
         setAllowedColors(new ArrayList<>(Arrays.asList(Color.BLACK, Color.CRIMSON)));
         this.color = getRandomColorFromAllowed();
     }
 
-    protected Color calcColor()
-    {
-        return getRandomColorFromAllowed();
-    }*/
+*/
 }
